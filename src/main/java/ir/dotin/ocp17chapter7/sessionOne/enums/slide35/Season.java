31fbe0007e7f0ace27095b2ae8cb45dee3223f69
Ali Norouzi <alinorouzi79@gmail.com>
2024-06-01 00:13:37 +0330

    Session One Completed

---
+++ src/main/java/ir/dotin/ocp17chapter7/sessionOne/enums/slide35/Season.java
@@ -0,0 +1,5 @@
+package ir.dotin.ocp17chapter7.sessionOne.enums.slide35;
+
+public enum Season {
+    WINTER, SPRING, SUMMER, FALL;
+}
---
+++ src/main/java/ir/dotin/ocp17chapter7/sessionOne/enums/slide36/ExtendedSeason.java
@@ -0,0 +1,12 @@
+package ir.dotin.ocp17chapter7.sessionOne.enums.slide36;
+
+import ir.dotin.ocp17chapter7.sessionOne.enums.slide35.Season;
+
+/**
+ * One thing that you can’t do is extend an enum.
+ * The values in an enum are fixed. You cannot add more by extending the enum.
+ */
+
+/*
+public enum ExtendedSeason extends Season {}
+*/
